8c04ac0a0c4791636eeb3b4ff8c35b2d86defa58
Sergey Goncharuk <fleshed914@gmail.com>
2017-04-02 00:27:34 +0300

    Описание инструмента
    Тестовый коммит для второй ветки проекта.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,6 @@
 app.controller('MainController', ['$scope', function($scope){
   $scope.title = 'Notebook';
+  $scope.describe = 'Notebook contains notes that user filled';
   $scope.notes = [ 
   { 
     title: 'Japan',  
